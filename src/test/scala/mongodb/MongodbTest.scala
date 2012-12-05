15b7486c1e4125aa3f0c6620fce34d8a6fa3e440
Tony Baines <tony.baines.1@gmail.com>
2012-12-05 18:02:39 +0000

    Initial working test inseting and querying Mongo

---
+++ src/test/scala/mongodb/MongodbTest.scala
@@ -0,0 +1,33 @@
+package mongodb
+
+import org.scalatest.{BeforeAndAfter, FunSuite}
+import com.mongodb.casbah.Imports._
+import org.joda.time.DateTime
+import com.mongodb.casbah.commons.conversions.scala.RegisterJodaTimeConversionHelpers
+
+class MongodbTest extends FunSuite with BeforeAndAfter {
+  val mongoConn = MongoConnection()
+  val databaseName: String = "testdb"
+  val collectionName: String = "cities"
+
+  before {
+    RegisterJodaTimeConversionHelpers()
+    mongoConn(databaseName).dropDatabase()
+    insertCity("Punxsutawney", 6200, new DateTime("2008-01-31"), List("phil the groundhog"), Map("name" -> "Jim Wehrle") )
+  }
+
+  test("querying from a Mongo collection") {
+    assert(mongoConn(databaseName)(collectionName).findOne().size > 0)
+  }
+
+  def insertCity( name: String, population: Int, last_census: DateTime, famous_for: List[String], mayor_info: Map[String,String] ) {
+    mongoConn(databaseName)(collectionName) += MongoDBObject(
+      "name" -> name,
+      "population" -> population,
+      "last_census" ->  last_census,
+      "famous_for" -> famous_for,
+      "mayor" -> mayor_info
+    )
+  }
+
+}
