a3fff3b37e56e29c4ba124d7fee0120a99c06421
Tony Baines <tony.baines.1@gmail.com>
2012-12-06 22:07:48 +0000

    Added a test for doing regex searches

--- src/test/scala/mongodb/MongodbTest.scala
+++ src/test/scala/mongodb/MongodbTest.scala
@@ -2,8 +2,10 @@ package mongodb
 
 import org.scalatest.{BeforeAndAfter, FunSuite}
 import com.mongodb.casbah.Imports._
+import com.mongodb.casbah.query.Implicits._
 import org.joda.time.DateTime
 import com.mongodb.casbah.commons.conversions.scala.RegisterJodaTimeConversionHelpers
+import com.mongodb.casbah
 
 class MongodbTest extends FunSuite with BeforeAndAfter {
   val mongoConn = MongoConnection()
@@ -13,15 +15,23 @@ class MongodbTest extends FunSuite with BeforeAndAfter {
   before {
     RegisterJodaTimeConversionHelpers()
     mongoConn(databaseName).dropDatabase()
+    insertCity("New York", 22200000, new DateTime("2009-07-31"), List("statue of liberty", "food"), Map("name" -> "Michael Bloomberg", "party" -> "I"))
     insertCity("Punxsutawney", 6200, new DateTime("2008-01-31"), List("phil the groundhog"), Map("name" -> "Jim Wehrle") )
     insertCity("Portland", 582000, new DateTime("2007-09-20"), List("beer", "food"), Map("name" -> "Sam Adams", "party" -> "D") )
   }
 
   test("querying from a Mongo collection") {
-    assert(mongoConn(databaseName)(collectionName).find().size === 2)
+    assert(mongoConn(databaseName)(collectionName).find().size === 3)
   }
 
-  def insertCity( name: String, population: Int, last_census: DateTime, famous_for: List[String], mayor_info: Map[String,String] ) {
+  test("querying using a regular expressions") {
+      val query = MongoDBObject("name" -> "(?i)new.*".r)
+      val result = mongoConn(databaseName)(collectionName).findOne(query).head
+      assert(result("name") === "New York")
+      println(result)
+   }
+
+   def insertCity( name: String, population: Int, last_census: DateTime, famous_for: List[String], mayor_info: DBObject ) {
     mongoConn(databaseName)(collectionName) += MongoDBObject(
       "name" -> name,
       "population" -> population,
